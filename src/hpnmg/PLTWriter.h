255e0dd4720d12f602a20f460e55be4ea2ea360a
Patricia92 <PatriciaSchinke92@gmail.com>
2018-03-08 13:21:56 +0100

    Add xml writer to write results in xml

--- src/hpnmg/PLTWriter.h
+++ src/hpnmg/PLTWriter.h
@@ -1,22 +1,31 @@
 #pragma once
 
-//#include <xercesc/dom/DOM.hpp>
-#include <xercesc/dom/DOMDocument.hpp>
-//#include <xercesc/dom/DOMDocumentType.hpp>
-//#include <xercesc/dom/DOMElement.hpp>
-//#include <xercesc/dom/DOMImplementation.hpp>
-//#include <xercesc/dom/DOMImplementationLS.hpp>
-//#include <xercesc/dom/DOMNodeIterator.hpp>
-//#include <xercesc/dom/DOMNodeList.hpp>
-//#include <xercesc/dom/DOMText.hpp>
+#include <xercesc/util/PlatformUtils.hpp>
+#include <xercesc/dom/DOM.hpp>
+#include <xercesc/util/XMLString.hpp>
+#include <xercesc/framework/StdOutFormatTarget.hpp>
+#include <xercesc/framework/LocalFileFormatTarget.hpp>
 
+#include "ParametricLocationTree.h"
 
 
 namespace hpnmg {
 
-    class WritePLT {
-    public:
-        WritePLT();
+    class PLTWriter {
+    private:
+        xercesc::DOMImplementation *domImplementation;
+        xercesc::DOMLSSerializer *serializer;
+        xercesc::XMLFormatTarget *target;
+        unsigned long idCounter = 1;
 
+    public:
+        PLTWriter();
+
+        void writePLT(shared_ptr<ParametricLocationTree> plt, double maxTime);
+
+        xercesc::DOMNode *
+        generateParametricLocationElement(shared_ptr<ParametricLocationTree> plt, ParametricLocationTree::Node node,
+                                          xercesc::DOMDocument *domNode);
+        std::string getEventName(EventType eventType);
     };
 }
