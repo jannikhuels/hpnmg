691e47bbecbd366a45a59aec4c60b9fa12b1f80e
Henner Niehaus <h_nieh06@uni-muenster.de>
2019-06-17 18:23:21 +0200

    Add test for Conjunction formulae

--- test/testRegionModelChecker.cpp
+++ test/testRegionModelChecker.cpp
@@ -68,13 +68,41 @@ TEST(RegionModelChecker, DiscreteAtomicPropertyTest2GT) {
 }
 
 TEST(RegionModelChecker, ConjunctionTest) {
-    auto modelChecker = RegionModelChecker(*ReadHybridPetrinet{}.readHybridPetrinet("example.xml"), 10);
-    auto formula = Formula(std::make_shared<Conjunction>(
+    auto modelChecker = RegionModelChecker(*ReadHybridPetrinet{}.readHybridPetrinet("example.xml"), 50);
+
+    auto result = modelChecker.satisfies(Formula(std::make_shared<Conjunction>(
         Formula(std::make_shared<DiscreteAtomicProperty>("pd1", 1)),
         Formula(std::make_shared<DiscreteAtomicProperty>("pd2", 1))
-    ));
-
-
-    const auto result = modelChecker.satisfies(formula, 0);
+    )), 0);
     EXPECT_NEAR(1.0, result.first, result.second);
+
+    result = modelChecker.satisfies(Formula(std::make_shared<Conjunction>(
+        Formula(std::make_shared<DiscreteAtomicProperty>("pd1", 1)),
+        Formula(std::make_shared<DiscreteAtomicProperty>("pd2", 1))
+    )), 2.5);
+    // folded normal distribution with mu = 5 and sigma = 3
+    // (1 - cdf(2.5)) = 1 - (0.5 * (erf((2.5 + 5) / sqrt(18)) + erf((2.5 - 5) / sqrt(18)))) ~ 0.803881
+    EXPECT_NEAR(0.803881, result.first, result.second);
+
+    result = modelChecker.satisfies(Formula(std::make_shared<Conjunction>(
+        Formula(std::make_shared<DiscreteAtomicProperty>("pd1", 0)),
+        Formula(std::make_shared<DiscreteAtomicProperty>("pd2", 1))
+    )), 2.5);
+    // folded normal distribution with mu = 5 and sigma = 3
+    // cdf(2.5) = 0.5 * (erf((2.5 + 5) / sqrt(18)) + erf((2.5 - 5) / sqrt(18))) ~ 0.196119
+    EXPECT_NEAR(0.196119, result.first, result.second);
+
+    result = modelChecker.satisfies(Formula(std::make_shared<Conjunction>(
+        Formula(std::make_shared<DiscreteAtomicProperty>("pd1", 1)),
+        Formula(std::make_shared<DiscreteAtomicProperty>("pd2", 1))
+    )), 6);
+    EXPECT_NEAR(0.0, result.first, result.second);
+
+    result = modelChecker.satisfies(Formula(std::make_shared<Conjunction>(
+        Formula(std::make_shared<DiscreteAtomicProperty>("pd1", 0)),
+        Formula(std::make_shared<DiscreteAtomicProperty>("pd2", 0))
+    )), 6);
+    // folded normal distribution with mu = 5 and sigma = 3
+    // cdf(6) = 0.5 * (erf((6 + 5) / sqrt(18)) + erf((6 - 5) / sqrt(18))) ~ 0.499571
+    EXPECT_NEAR(0.630436, result.first, result.second);
 }
