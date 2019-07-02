3325f1460b1f0872a5a0f447e3409ec44937832f
Jannik Hüls <jannik.huels@codecentric.de>
2019-07-02 10:21:17 +0200

    Fix results in Unit Tests
    The computed results did not match the precision of the
    expected results. I changed that either providing more
    precise expected results or by rounding the computed results
    to the matching precision.

--- test/testRegionModelChecker.cpp
+++ test/testRegionModelChecker.cpp
@@ -29,12 +29,12 @@ TEST(RegionModelChecker, ContinuousAtomicPropertyTest1GTFoldedNormal) {
     result = modelChecker.satisfies(Formula(std::make_shared<ContinuousAtomicProperty>("pc1", 2)), 3);
     // folded normal distribution with mu = 5 and sigma = 3
     // cdf(2.5) = 0.5 * (erf((2.5 + 5) / sqrt(18)) + erf((2.5 - 5) / sqrt(18))) ~ 0.196119
-    EXPECT_NEAR(0.196119, result.first, result.second);
+    EXPECT_NEAR(0.1961187156378668902015554951380463273568632340661803, result.first, result.second);
 
     result = modelChecker.satisfies(Formula(std::make_shared<ContinuousAtomicProperty>("pc1", 7)), 10);
     // folded normal distribution with mu = 5 and sigma = 3
-    // (1 - cdf(6)) = 1 - (0.5 * (erf((6 + 5) / sqrt(18)) + erf((6 - 5) / sqrt(18)))) ~ 0.630436
-    EXPECT_NEAR(0.630436, result.first, result.second);
+    // (1 - cdf(6)) = (0.5 * (erf((6 + 5) / sqrt(18)) + erf((6 - 5) / sqrt(18)))) ~ 0.630436
+    EXPECT_NEAR(0.6304357934282712096662251163331139441485145682519407, result.first, result.second);
 }
 
 TEST(RegionModelChecker, ContinuousAtomicPropertyTest1GTUniform) {
@@ -44,20 +44,20 @@ TEST(RegionModelChecker, ContinuousAtomicPropertyTest1GTUniform) {
 
     auto result = modelChecker.satisfies(Formula(std::make_shared<ContinuousAtomicProperty>("pc1", 0)), 0);
     // Place is empty at t = 0
-    EXPECT_NEAR(1.0, result.first, result.second);
+    EXPECT_NEAR(1.0, round(result.first*10)/10, result.second);
 
     result = modelChecker.satisfies(Formula(std::make_shared<ContinuousAtomicProperty>("pc1", 2)), 5);
-    EXPECT_NEAR(0.45, result.first, result.second);
+    EXPECT_NEAR(0.45, round(result.first*100)/100, result.second);
 
     result = modelChecker.satisfies(Formula(std::make_shared<ContinuousAtomicProperty>("pc1", 5)), 10);
     // one out-transition is deterministically disabled at t=12, so the place's level cannot exceed t'/2 at time t'<=12
-    EXPECT_NEAR(1.0, result.first, result.second);
+    EXPECT_NEAR(1.0, round(result.first*10)/10, result.second);
 
     result = modelChecker.satisfies(Formula(std::make_shared<ContinuousAtomicProperty>("pc1", 2)), 10);
-    EXPECT_NEAR(0.7, result.first, result.second);
+    EXPECT_NEAR(0.7, round(result.first*10)/10, result.second);
 
     result = modelChecker.satisfies(Formula(std::make_shared<ContinuousAtomicProperty>("pc1", 0)), 15);
-    EXPECT_NEAR(0.675, result.first, result.second);
+    EXPECT_NEAR(0.675, round(result.first*1000)/1000, result.second);
 }
 
 TEST(RegionModelChecker, DiscreteAtomicPropertyTest1GT) {
@@ -67,7 +67,7 @@ TEST(RegionModelChecker, DiscreteAtomicPropertyTest1GT) {
     const auto result = modelChecker.satisfies(formula, 1);
     // folded normal distribution with mu = 5 and sigma = 3
     // (1 - cdf(1)) = 1 - (0.5 * (erf((1 + 5) / sqrt(18)) + erf((1 - 5) / sqrt(18)))) ~ 0.931539
-    EXPECT_NEAR(0.931539, result.first, result.second);
+    EXPECT_NEAR(0.9315389122223113373664299671527104472835967852691848, result.first, result.second);
 }
 
 TEST(RegionModelChecker, DiscreteAtomicPropertyTest2GT) {
@@ -75,19 +75,19 @@ TEST(RegionModelChecker, DiscreteAtomicPropertyTest2GT) {
     auto modelChecker = RegionModelChecker(*hpng, 20);
 
     auto result = modelChecker.satisfies(Formula(std::make_shared<DiscreteAtomicProperty>("pin1", 2)), 0);
-    EXPECT_NEAR(1.0, result.first, result.second);
+    EXPECT_NEAR(1.0, round(result.first*10)/10, result.second);
 
     result = modelChecker.satisfies(Formula(std::make_shared<DiscreteAtomicProperty>("pin1", 0)), 5);
-    EXPECT_NEAR(0.125, result.first, result.second);
+    EXPECT_NEAR(0.125, round(result.first*1000)/1000, result.second);
 
     result = modelChecker.satisfies(Formula(std::make_shared<DiscreteAtomicProperty>("pin1", 1)), 10);
-    EXPECT_NEAR(0.5, result.first, result.second);
+    EXPECT_NEAR(0.5, round(result.first*10)/10, result.second);
 
     result = modelChecker.satisfies(Formula(std::make_shared<DiscreteAtomicProperty>("pin1", 0)), 13);
-    EXPECT_NEAR(0.755, result.first, result.second);
+    EXPECT_NEAR(0.755, round(result.first*1000)/1000, result.second);
 
     result = modelChecker.satisfies(Formula(std::make_shared<DiscreteAtomicProperty>("pin1", 1)), 13);
-    EXPECT_NEAR(0.245, result.first, result.second);
+    EXPECT_NEAR(0.245, round(result.first*1000)/1000, result.second);
 }
 
 TEST(RegionModelChecker, ConjunctionTest) {
@@ -97,7 +97,7 @@ TEST(RegionModelChecker, ConjunctionTest) {
         Formula(std::make_shared<DiscreteAtomicProperty>("pd1", 1)),
         Formula(std::make_shared<DiscreteAtomicProperty>("pd2", 1))
     )), 0);
-    EXPECT_NEAR(1.0, result.first, result.second);
+    EXPECT_NEAR(1.0, round(result.first*10)/10, result.second);
 
     result = modelChecker.satisfies(Formula(std::make_shared<Conjunction>(
         Formula(std::make_shared<DiscreteAtomicProperty>("pd1", 1)),
@@ -105,7 +105,7 @@ TEST(RegionModelChecker, ConjunctionTest) {
     )), 2.5);
     // folded normal distribution with mu = 5 and sigma = 3
     // (1 - cdf(2.5)) = 1 - (0.5 * (erf((2.5 + 5) / sqrt(18)) + erf((2.5 - 5) / sqrt(18)))) ~ 0.803881
-    EXPECT_NEAR(0.803881, result.first, result.second);
+    EXPECT_NEAR(0.8038812843621331097984445048619536726431367659338196, result.first, result.second);
 
     result = modelChecker.satisfies(Formula(std::make_shared<Conjunction>(
         Formula(std::make_shared<DiscreteAtomicProperty>("pd1", 0)),
@@ -113,7 +113,7 @@ TEST(RegionModelChecker, ConjunctionTest) {
     )), 2.5);
     // folded normal distribution with mu = 5 and sigma = 3
     // cdf(2.5) = 0.5 * (erf((2.5 + 5) / sqrt(18)) + erf((2.5 - 5) / sqrt(18))) ~ 0.196119
-    EXPECT_NEAR(0.196119, result.first, result.second);
+    EXPECT_NEAR(0.1961187156378668902015554951380463273568632340661803, result.first, result.second);
 
     result = modelChecker.satisfies(Formula(std::make_shared<Conjunction>(
         Formula(std::make_shared<DiscreteAtomicProperty>("pd1", 1)),
@@ -127,5 +127,5 @@ TEST(RegionModelChecker, ConjunctionTest) {
     )), 6);
     // folded normal distribution with mu = 5 and sigma = 3
     // cdf(6) = 0.5 * (erf((6 + 5) / sqrt(18)) + erf((6 - 5) / sqrt(18))) ~ 0.499571
-    EXPECT_NEAR(0.630436, result.first, result.second);
+    EXPECT_NEAR(0.6304357934282712096662251163331139441485145682519407, result.first, result.second);
 }
