c07e2f98549007e9b8610ab9ad5caa7095893636
Jannik Hüls <jannik.huels@codecentric.de>
2019-09-18 15:59:04 +0200

    Cleanup of main.cpp
    There was a small bug that produces an error when no
    parameter was specified. Furthermore the old main function
    was uncommented and is now deleted.

--- main.cpp
+++ main.cpp
@@ -40,7 +40,7 @@ int process_command_line(int argc, char **argv, std::string& model, std::string&
 
         if (argc == 1) {
             cout << endl << "No option was specified." << endl << endl << desc << endl;
-            return 1;
+            return 2;
         }
 
         po::variables_map vm;
@@ -49,7 +49,7 @@ int process_command_line(int argc, char **argv, std::string& model, std::string&
 
         if (vm.count("help")) {
             cout << desc << endl;
-            return 1;
+            return 2;
         }
 
         if (vm.count("stateonly")) {
@@ -168,147 +168,3 @@ int main (int argc, char *argv[]) {
 
     return 0;
 }
-
-/*int main (int argc, char *argv[]) {
-
-    using namespace hpnmg;
-
-    fstream resultfile {RESULT_FILE, resultfile.out | resultfile.app};
-
-    if (argc == 1)
-        throw std::runtime_error("no parameters specified.");
-
-    std::map<std::string,std::string> parameterIndexMap;
-    parameterIndexMap[MODE_PARAMETER] = argv[1];
-
-    const auto mode = [&argv, argc, &parameterIndexMap]() {
-        errno = 0;
-        auto result = std::string(parameterIndexMap.find(MODE_PARAMETER)->second);
-        if (result == "-s") {
-            if (argc<4) {
-                throw std::invalid_argument("invalid number of parameters specified.");
-            }
-            parameterIndexMap[MODEL_PARAMETER] = argv[2];
-            parameterIndexMap[MAXTIME_PARAMETER] = argv[3];
-            return statespaceonly;
-        }
-        if (result == "-m") {
-            if (argc<6) {
-                throw std::invalid_argument("invalid number of parameters specified.");
-            }
-            parameterIndexMap[MODEL_PARAMETER] = argv[2];
-            parameterIndexMap[FORMULA_PARAMETER] = argv[3];
-            parameterIndexMap[CHECKTIME_PARAMETER] = argv[4];
-            parameterIndexMap[MAXTIME_PARAMETER] = argv[5];
-            return modelchecking;
-        }
-
-        throw std::invalid_argument("unknown mode specified.");
-    }();
-
-
-
-    const auto modelFile = std::string(argv[parameterIndexMap.find(MODEL_PARAMETER)->second]);
-
-    const auto maxTime = [&argv,mode,&parameterIndexMap]() {
-        errno = 0;
-        auto result = std::strtod(argv[parameterIndexMap.find(MAXTIME_PARAMETER)->second], nullptr);
-        if (errno != 0) {
-            std::string val = std::string(argv[parameterIndexMap.find(MAXTIME_PARAMETER)->second]);
-            throw std::invalid_argument(std::string("bad max time: ") + val);
-        }
-
-        return result;
-    }();
-
-    auto checkTime = 0;
-    if (mode == modelchecking) {
-        checkTime = [&argv, &parameterIndexMap]() {
-            errno = 0;
-            auto result = std::strtod(argv[parameterIndexMap.find(CHECKTIME_PARAMETER)->second], nullptr);
-            if (errno != 0)
-                throw std::invalid_argument(std::string("bad check time: ") + std::string(argv[parameterIndexMap.find(CHECKTIME_PARAMETER)->second]));
-
-            return result;
-        }();
-    }
-
-    const auto startTotal = std::chrono::high_resolution_clock::now();
-
-    const auto startRead = std::chrono::high_resolution_clock::now();
-    auto hpng = ReadHybridPetrinet{}.readHybridPetrinet(modelFile);
-
-    const auto endRead = std::chrono::high_resolution_clock::now();
-    auto readingTime = std::chrono::duration_cast<std::chrono::milliseconds>(endRead - startRead).count();
-    std::cout << "Reading took " << readingTime << "ms." << std::endl;
-
-    const auto startChecker = std::chrono::high_resolution_clock::now();
-    auto checker = RegionModelChecker(*hpng, maxTime);
-    const auto endChecker = std::chrono::high_resolution_clock::now();
-    auto initTime = std::chrono::duration_cast<std::chrono::milliseconds>(endChecker - startChecker).count();
-    std::cout << "Initializing the checker took " << initTime << "ms." << std::endl;
-
-    if (mode == statespaceonly) {
-
-        measurefile << readingTime << "\t" << initTime;
-        // Append additional parameters to the output file
-        if (argc > 4) {
-            for (int i = 4; i < argc; i++) {
-                measurefile << "\t" << std::string(argv[i]);
-            }
-        }
-        measurefile << endl;
-
-        //    const auto startWrite = std::chrono::high_resolution_clock::now();
-//    auto plt = ParseHybridPetrinet{}.parseHybridPetrinet(hpng, maxTime);
-//    plt->updateRegions();
-//    PLTWriter{}.writePLT(plt, maxTime);
-//    const auto endWrite = std::chrono::high_resolution_clock::now();
-//    std::cout << "Parsing and writing took " << std::chrono::duration_cast<std::chrono::milliseconds>(endWrite - startWrite).count() << "ms." << std::endl;
-//
-//    auto ws = std::vector<ParametricLocationTree::Node>{plt->getRootNode()};
-//    while (!ws.empty()) {
-//        const auto node = ParametricLocationTree::Node(ws.back());
-//        ws.pop_back();
-//        auto childNodes = plt->getChildNodes(node);
-//        ws.insert(ws.end(), childNodes.begin(), childNodes.end());
-//
-//        std::cout << "node: " << node.getNodeID() << STDPolytope<mpq_class>(STDPolytope<mpq_class>(node.getRegion()).timeSlice(checkTime)) << std::endl;
-//    }
-
-        return 0;
-    }
-
-    const auto startSatisfy = std::chrono::high_resolution_clock::now();
-
-    const auto formulaFile = std::string(argv[3]);
-    Formula formula = ReadFormula{}.readFormula(formulaFile);
-
-    const auto checkResult = checker.satisfies(formula, checkTime);
-    const auto endSatisfy = std::chrono::high_resolution_clock::now();
-    std::cout << "Checking the formula took " << std::chrono::duration_cast<std::chrono::milliseconds>(endSatisfy - startSatisfy).count() << "ms." << std::endl;
-    std::cout << "The result (probability, error): (" << checkResult.first << ", " << checkResult.second << ")." << std::endl;
-
-//    const auto startSerialize = std::chrono::high_resolution_clock::now();
-//    // create and open a character archive for output
-//    std::ofstream ofs("checker_archive.txt");
-//    boost::archive::text_oarchive{ofs} << checker;
-//    const auto endSerialize = std::chrono::high_resolution_clock::now();
-//    std::cout << "Serializing took " << std::chrono::duration_cast<std::chrono::milliseconds>(endParse - startParse).count() << "ms." << std::endl;
-
-    const auto endTotal = std::chrono::high_resolution_clock::now();
-    std::cout << "Total time: " << std::chrono::duration_cast<std::chrono::milliseconds>(endTotal - startTotal).count() << "ms."
-        << "(" << std::chrono::duration_cast<std::chrono::seconds>(endTotal - startTotal).count() << "s)" << std::endl;
-
-    resultfile << checkTime << "\t" << checkResult.first << "\t" << checkResult.second << "\t" << std::chrono::duration_cast<std::chrono::milliseconds>(endTotal - startTotal).count();
-
-    if (argc > 5) {
-        for (int i = 4; i < argc; i++) {
-            resultfile << "\t" << std::string(argv[i]);
-        }
-    }
-    resultfile << endl;
-
-    resultfile.close();
-    return 0;
-}*/
