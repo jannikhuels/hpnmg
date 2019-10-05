c2b7054e35eb3531cf326c0e89b50dca48b98153
joannadelicaris <joannadelicaris@gmail.com>
2019-10-05 16:23:45 +0200

    added rectangular automaton test to testHybridAutomata

--- test/testHybridAutomata.cpp
+++ test/testHybridAutomata.cpp
@@ -839,3 +839,109 @@ TEST(HybridAutomaton, FlowParser){
         plotter.plotTex();
 
 }
+
+TEST(HybridAutomaton, RectangularAutomaton) {
+using namespace hypro;
+
+// typedefs for simplification.
+typedef mpq_class Number;
+typedef HPolytope<Number> Representation;
+
+// create the discrete structure of the automaton and the automaton itself.
+unique_ptr<Location<Number>> loc1 = make_unique<Location<Number>>();
+HybridAutomaton<Number> rectangularAutomaton = HybridAutomaton<Number>();
+
+
+// location parameter setting.
+
+// creation of the invariant.
+matrix_t<Number> invariantMat = matrix_t<Number>(1,2);
+vector_t<Number> invariantVec = vector_t<Number>(1);
+invariantVec(0) = Number(0);
+invariantMat(0,0) = Number(-1);
+invariantMat(0,1) = Number(0);
+Condition<Number> inv(invariantMat,invariantVec);
+loc1->setInvariant(inv);
+
+// setup rectangular flow
+auto& varpool = VariablePool::getInstance();
+rectangularFlow<Number> rectFlow;
+rectFlow.setFlowIntervalForDimension(carl::Interval<Number>{1,1},varpool.newCarlVariable("t"));
+rectFlow.setFlowIntervalForDimension(carl::Interval<Number>{1,3},varpool.newCarlVariable("x"));
+loc1->setRectangularFlow(rectFlow);
+
+
+/*
+// setup flow matrix (3x3, we add an artificial dimension to cope with constants).
+// matrix defining the flow (note: 3rd dimension for constant parts).
+matrix_t<Number> flowMatrix = matrix_t<Number>(3,3);
+flowMatrix(0,0) = Number(0);
+flowMatrix(0,1) = Number(1);
+flowMatrix(0,2) = Number(0);
+flowMatrix(1,0) = Number(0);
+flowMatrix(1,1) = Number(0);
+flowMatrix(1,2) = Number(carl::rationalize<Number>(-9.81));
+flowMatrix(2,0) = Number(0);
+flowMatrix(2,1) = Number(0);
+flowMatrix(2,2) = Number(0);
+loc1->setFlow(flowMatrix);
+ */
+
+
+// setup of the transition.
+
+
+// create Box holding the initial set.
+
+matrix_t<Number> boxMat = matrix_t<Number>(4,2);
+vector_t<Number> boxVec = vector_t<Number>(4);
+boxVec(0) = Number(carl::rationalize<Number>(10.2));
+boxVec(1) = Number(-10);
+boxVec(2) = Number(0);
+boxVec(3) = Number(0);
+boxMat(0,0) = Number(1);
+boxMat(0,1) = Number(0);
+boxMat(1,0) = Number(-1);
+boxMat(1,1) = Number(0);
+boxMat(2,0) = Number(0);
+boxMat(2,1) = Number(1);
+boxMat(3,0) = Number(0);
+boxMat(3,1) = Number(-1);
+
+// create initial state.
+rectangularAutomaton.addInitialState(loc1.get(), Condition<Number>(boxMat,boxVec));
+rectangularAutomaton.addLocation(move(loc1));
+
+
+// instanciate reachability analysis algorithm.
+hypro::reachability::Reach<Number, hypro::reachability::ReachSettings, hypro::State_t<Number>> reacher(rectangularAutomaton);
+ReachabilitySettings settings = reacher.settings();
+settings.timeStep = carl::convert<double,Number>(0.01);
+settings.timeBound = Number(3); //time bound
+settings.jumpDepth = 3;
+reacher.setSettings(settings);
+reacher.setRepresentationType(Representation::type());
+
+// perform reachability analysis.
+auto flowpipeIndices = reacher.computeForwardReachability();
+
+// plot flowpipes.
+Plotter<Number>& plotter = Plotter<Number>::getInstance();
+plotter.setFilename("bouncingBall");
+for(auto& indexPair : flowpipeIndices){
+std::vector<hypro::State_t<Number>> flowpipe = indexPair.second;
+// Plot single flowpipe
+for(auto& set : flowpipe) {
+std::vector<Point<Number>> points = set.vertices();
+if(!points.empty() && points.size() > 2) {
+for(auto& point : points) {
+point.reduceDimension(2);
+}
+plotter.addObject(points);
+points.clear();
+}
+}
+}
+// write output.
+plotter.plotTex();
+}
